20a87db14123640a8b6e9eca5e5dcd8a8928e964
gautambafnaFM <gautam.bafna@flairminds.com>
2025-07-16 19:43:52 +0530

    changes regarding project

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,8 +7,8 @@ export const projects = [
     projectName: "Swift Meetup",
     ProjectCategorie: "Video call Full stack App",
     ProjectDetails: "Pioneered seamless video communication with LiveKit",
-    liveLink: "https://swift-meet.vercel.app/",
-    repo: "https://github.com/ajinkyajagdale21/swiftMeet",
+    liveLink: "https://swift-meetup.vercel.app/",
+    repo: "https://github.com/gautambafna26/swiftMeetup",
     TechStack: ["ReactJs","Live Kit", "Auth 0","Node JS","Express JS","Bootstrap"],
   },
   {
@@ -22,61 +22,8 @@ export const projects = [
     repo: "https://github.com/ajinkyajagdale21/swiftGPT",
     TechStack: ["Open AI","ReactJs", "Auth 0","Tailwind CSS"],
   },
-  {
-    pid: ++id,
-    image:
-      "https://cdn.pixabay.com/photo/2018/03/14/01/03/subscribe-3224137_960_720.jpg",
-    projectName: "customizable-react-audio-player",
-    ProjectCategorie: "NPM Package",
-    ProjectDetails: "customizable-react-audio-player allows you to change styles and colors to your react audio player with ease.",
-    liveLink: "https://www.npmjs.com/package/customizable-react-audio-player",
-    repo: "https://github.com/ajinkyajagdale21/customized-react-audio-player",
-    TechStack: ["Npm","ReactJs", "Material UI"],
-  },
-  {
-    pid: ++id,
-    image:
-      "https://cdn.pixabay.com/photo/2018/03/14/01/03/subscribe-3224137_960_720.jpg",
-    projectName: "SwiftFlix",
-    ProjectCategorie: "VideoLibrary App",
-    ProjectDetails: "VideoLibrary Web App Made Using",
-    liveLink: "https://swiftflix.netlify.app/",
-    repo: "https://github.com/ajinkyajagdale21/video-player/tree/dev",
-    TechStack: ["ReactJs", "MongoDB", "ExpressJS", "NodeJS"],
-  },
-  {
-    pid: ++id,
-    image:
-      "https://cdn.pixabay.com/photo/2017/01/19/13/22/ecommerce-1992280_960_720.png",
-    projectName: "PeachCart App",
-    ProjectCategorie: "Ecommerce",
-    liveLink: "https://peachcart21.netlify.app/",
-    repo: "https://github.com/ajinkyajagdale21/peachcart/tree/development",
-    ProjectDetails: "Ecommerce Web App Made Using",
-    TechStack: ["ReactJs", "MongoDB", "ExpressJS", "NodeJS"],
-  },
-  {
-    pid: ++id,
-    image:
-      "https://cdn.pixabay.com/photo/2016/11/19/23/00/css3-1841590_960_720.png",
-    projectName: "PeachCSS",
-    ProjectCategorie: "Component Library",
-    liveLink: "https://peachcss.netlify.app/",
-    repo: "https://github.com/ajinkyajagdale21/PeachCSS-Component-Library",
-    ProjectDetails: "ComponentLibrary Made Using",
-    TechStack: ["Vanilla CSS"],
-  },
-  {
-    pid: ++id,
-    image:
-      "https://cdn.pixabay.com/photo/2015/01/20/13/13/samsung-605439__340.jpg",
-    projectName: "Spotunes",
-    ProjectCategorie: "Music App",
-    liveLink: "https://aji21-music-player.netlify.app/",
-    repo: "https://github.com/ajinkyajagdale21/music-player",
-    ProjectDetails: "Music App Made Using",
-    TechStack: ["ReactJS"],
-  },
+ 
+  
   {
     pid: ++id,
     image:
